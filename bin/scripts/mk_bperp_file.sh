905ba738c1fe063c15aff45975cce0671a305cb0
Milan Lazecky <espiritocz@gmail.com>
2019-08-08 18:18:42 +0100

    updated baselines file generation

--- bin/scripts/mk_bperp_file.sh
+++ bin/scripts/mk_bperp_file.sh
@@ -14,42 +14,64 @@ tmpdir="RSLCtmp"
 rm -rf $tmpdir
 mkdir $tmpdir
 
+parcode=`ls RSLC/$master/*rslc.par | head -n1 | cut -d '.' -f2`
+#parcode is e.g. IW1
+
 ## for each RSLC date
 rm -f SLC_mosaics_tab
 while read day; do
   rslc="$day/${day}.rslc"
-  par="$day/${day}.rslc.par"
+  par="$day/${day}.${parcode}.rslc.par"
+
+  ## If 7zipped
+  if [ -f RSLC/${day}.7z ]; then
+    #echo "Extract rslc.par for $day"
+    ## Extract only rslc.par file
+    #par=`7za l RSLC/${day}.7z | grep rslc.par | head -n1 | rev | gawk {'print $1'} | rev`
+    #7za x '-i!????????/????????.rslc.par' -o$tmpdir RSLC/${day}.7z >/dev/null 2>/dev/null
+    7za x -o$tmpdir RSLC/${day}.7z $par >/dev/null 2>/dev/null
+    #if [ -f $tmpdir/$par ] ;then ## If succeed
+    #  echo "$tmpdir/$day/${day}.rslc $tmpdir/$day/${day}.rslc.par" >> SLC_mosaics_tab
+    #fi
 
   ## If not 7zipped
-  if [ -f RSLC/$par ]; then
+  elif [ -f RSLC/$par ]; then
+    #par=`ls $day/${day}*rslc.par | head -n1`
     #echo "Link $par to $tmpdir"
     mkdir $tmpdir/$day
     cd $tmpdir/$day
     ln -s ../../RSLC/$par ./
     cd ../../
-    echo "$tmpdir/$day/${day}.rslc $tmpdir/$day/${day}.rslc.par" >> SLC_mosaics_tab
-
-  ## If 7zipped
-  elif [ -f RSLC/${day}.7z ]; then
-    #echo "Extract rslc.par for $day"
-    ## Extract only rslc.par file
-    7za x '-i!????????/????????.rslc.par' -o$tmpdir RSLC/${day}.7z
-
-    if [ -f $tmpdir/$par ] ;then ## If succeed
-      echo "$tmpdir/$day/${day}.rslc $tmpdir/$day/${day}.rslc.par" >> SLC_mosaics_tab
-    fi
-
+    #echo "$tmpdir/$day/${day}.rslc $tmpdir/$day/${day}.rslc.par" >> SLC_mosaics_tab
   fi
 
 done < rslc.list
 
-## Make bperp_file wrt single master
-base_calc SLC_mosaics_tab $tmpdir/${master}/${master}.rslc.par bperp_file_tmp itab 0 >/dev/null 2>/dev/null
+#removing the old file..
+#rm bperp_file 2>/dev/null
+rm baselines  2>/dev/null
 
+## Make baselines file wrt single master
+for image in `cat rslc.list`; do
+  bperp=`base_orbit $tmpdir/${master}/${master}.$parcode.rslc.par $tmpdir/$image/$image.$parcode.rslc.par - 2>/dev/null | grep perpendicular | gawk {'print $5'}`
+ if [ ! -z $bperp ]; then
+  btemp=`datediff $master $image`
+  if [ $image -lt $master ]; then btemp="-"$btemp; fi
+  echo $master" "$image" "$bperp" "$btemp >> baselines
+ else
+  echo "seems as erroneous rslc: "`pwd`/RSLC/$image
+ fi
+done
+
+## Make bperp_file wrt single master
+#base_calc SLC_mosaics_tab $tmpdir/${master}/${master}.rslc.par bperp_file_tmp itab 0 >/dev/null 2>/dev/null
+
+ 
 ## Add 8th and 9th column
-awk '{printf "%4d %8d %8d %7.2f %6.1f %6.1f %6.1f %7.2f %7.2f\n",$1,$2,$3,$4,$5,0,$5,0,$4}' bperp_file_tmp > bperp_file_old
-awk '{printf "%8d %8d %7.2f %6.1f\n",$2,$3,$4,$5}' bperp_file_tmp > bperp_file
+#awk '{printf "%4d %8d %8d %7.2f %6.1f %6.1f %6.1f %7.2f %7.2f\n",$1,$2,$3,$4,$5,0,$5,0,$4}' bperp_file_tmp > bperp_file_old
+#awk '{printf "%8d %8d %7.2f %6.1f\n",$2,$3,$4,$5}' bperp_file_tmp > bperp_file
 
 ## Remove intermediate files
-rm -f itab rslc.list SLC_mosaics_tab base_calc.log base.out bperp_file_tmp
+#rm -f itab rslc.list SLC_mosaics_tab base_calc.log base.out bperp_file_tmp
+rm rslc.list 
 rm -rf $tmpdir
--- bin/scripts/update_bperp_file.sh
+++ bin/scripts/update_bperp_file.sh
@@ -5,31 +5,31 @@ frame=`pwd | rev | cut -d '/' -f1 | rev`
 if [ ! `echo $frame | cut -d '_' -f3 | wc -c` -eq 7 ]; then echo "to be run from frame directory"; exit; fi
 
 tr=`echo $frame | cut -d '_' -f1 | sed 's/^0//' | sed 's/^0//' | rev | cut -c 2- | rev`
-pub_bperp=$LiCSAR_public/$tr/$frame/metadata/bperp_file
+pub_bperp=$LiCSAR_public/$tr/$frame/metadata/baselines
 
-#regenerate bperp_file if it exists:
-if [ -f bperp_file ]; then
- mv bperp_file bperp_file.bck
+#regenerate baselines file if it exists:
+if [ -f baselines ]; then
+ mv baselines baselines.bck
 fi
 
-echo "preparing bperp file"
+echo "preparing baselines file"
 mk_bperp_file.sh
 
 if [ ! -f $pub_bperp ]; then
- echo "generating bperp_file in "$pub_bperp
+ echo "generating baselines file in "$pub_bperp
  #echo "master    slave     bperp   btemp" > $pub_bperp
- cp bperp_file $pub_bperp
+ cp baselines $pub_bperp
 else
- echo "updating bperp_file in "$pub_bperp
+ echo "updating baselines file in "$pub_bperp
  #tail -n+2 $pub_bperp > temp_bperp_file
- cp $pub_bperp temp_bperp_file
- cat bperp_file >> temp_bperp_file
+ cp $pub_bperp temp_baselines
+ cat baselines >> temp_baselines
  #echo "master    slave     bperp   btemp" > $pub_bperp
- sort -u temp_bperp_file >> $pub_bperp
- rm temp_bperp_file
+ sort -u temp_baselines >> $pub_bperp
+ rm temp_baselines
 fi
 
 #return the original bperp_file (in case someone wanted to use it)
-if [ -f bperp_file.bck ]; then
- mv bperp_file.bck bperp_file
+if [ -f baselines.bck ]; then
+ mv baselines.bck baselines
 fi
