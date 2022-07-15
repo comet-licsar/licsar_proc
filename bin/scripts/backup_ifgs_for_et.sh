020592b7d3ae0f7188b25ee005afe11884fdecd3
laz048 <milan.lazecky@vsb.cz>
2022-07-15 16:31:52 +0100

    regular update

--- bin/scripts/backup_ifgs_for_et.sh
+++ bin/scripts/backup_ifgs_for_et.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+outdir=/gws/pw/j05/comet_lics/backup_2022_IFG
 
 cd $LiCSAR_procdir
+for tr in `seq 1 175`; do
+ for frame in `ls $tr`; do 
+   if [ `ls $tr/$frame/IFG 2>/dev/null | wc -l` != 0 ]; then
+    echo $frame
+    mkdir -p $outdir/$tr/$frame/IFG
+    mv $tr/$frame/IFG/* $outdir/$tr/$frame/IFG/.
+   fi
+ done
+done
