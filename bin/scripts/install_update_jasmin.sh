936bb972b39699b3a9706f61348c259fa43a5c04
Milan Lazecky <espiritocz@gmail.com>
2022-11-22 13:33:20 +0000

    regular update

--- bin/scripts/install_update_jasmin.sh
+++ bin/scripts/install_update_jasmin.sh
@@ -15,9 +15,14 @@ sleep 2
 
 echo "setting new default path to BATCH_CACHE_DIR and creating it: "
 source ~/.bashrc
-oldbatch=$BATCH_CACHE_DIR
 sed -i 's/export BATCH_CACHE_DIR/\#export BATCH_CACHE_DIR/' ~/.bashrc
-echo "oldbatch="$oldbatch >> ~/.bashrc
+
+if [ `grep -c "export oldbatch=" ~/.bashrc` -lt 1 ]; then
+ echo "export oldbatch="$oldbatch >> ~/.bashrc
+ oldbatch=$BATCH_CACHE_DIR
+ echo "export BATCH_CACHE_DIR=/work/scratch-pw3/licsar/"$USER"/batchdir" >> ~/.bashrc
+fi
+
 
 export BATCH_CACHE_DIR=/work/scratch-pw3/licsar/$USER/batchdir
 export LiCSAR_temp=/work/scratch-pw3/licsar/$USER/LiCSAR_temp
@@ -36,7 +41,7 @@ else
  cd $oldbatch;
  for x in `ls`; do
   echo "copying "$x;
-  rsync -r $x $BATCH_CACHE_DIR
+  rsync -r $x $BATCH_CACHE_DIR 2>/dev/null
  done
 fi
 
