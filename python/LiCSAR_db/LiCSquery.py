4f574cf111a6423eae2ca81bb15be581cf414fd7
Milan Lazecky <espiritocz@gmail.com>
2022-12-06 15:50:53 +0000

    regular update

--- python/LiCSAR_db/LiCSquery.py
+++ python/LiCSAR_db/LiCSquery.py
@@ -1076,7 +1076,7 @@ def get_daz(polyid, epoch):
 
 
 def ingest_esd(frame, epoch, rslc3, daz, ccazi, ccrg, orb, overwrite = True):
-	"""Function to import ESD (etc.) values to the database
+    """Function to import ESD (etc.) values to the database
 	
 	Args:
 		frame (str): 	frame ID
@@ -1087,16 +1087,16 @@ def ingest_esd(frame, epoch, rslc3, daz, ccazi, ccrg, orb, overwrite = True):
 		ccrg (float):	$\Delta r_{ICC}$ [px] offset from intensity/incoherent cross-correlation (ICC) in range
 		orb (str):		orbit file used here (e.g. S1A_POE_.....zip) - special value 'fixed_as_in_GRL' means imported from older data and fixed
 	
-	"""
+    """
     polyid = sqlout2list(get_frame_polyid(frame))[0]
     if get_daz(polyid, epoch):
-		if overwrite:
-			#clean it first
-			sql_q = "delete from esd where polyid={} and epoch='{}';".format(polyid, epoch)
-			res = do_query(sql_q, 1)
+        if overwrite:
+            #clean it first
+            sql_q = "delete from esd where polyid={} and epoch='{}';".format(polyid, epoch)
+            res = do_query(sql_q, 1)
         else:
-			print('the record exists, skipping')
-			return
+            print('the record exists, skipping')
+            return
     #the DATE in MySQL is pretty flexible... so using just the values directly:
     sql_q = "insert into esd values ({}, '{}', '{}', '{}', {}, {}, {})".format(polyid, epoch, rslc3, orb, daz, ccazi, ccrg)
     res = do_query(sql_q, 1)
