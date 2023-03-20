2e16281e4f0fdf2cafa5db9f6f2089b6a89bd373
laz048 <milan.lazecky@vsb.cz>
2023-03-20 13:04:18 +0000

    regular update

---
+++ bin/scripts/download_s1_and_ingest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#this script will download a given file (zip file) from ASF to LiCSAR_SLC and ingest it to db
+
+if [ -z $1 ]; then echo "Parameter is the file name, e.g. S1A....zip"; exit; fi
+if [ `echo $1 | grep -c zip` -eq 0 ]; then echo "Parameter is the file name, e.g. S1A....zip"; exit; fi
+zipf=$1
+
+cd $LiCSAR_SLC
+wget_alaska $zipf
+arch2DB -f `pwd`/$zipf
+cd -
