32de9cdd96076e3602004fbd7e250722055c20e2
Milan Lazecky <espiritocz@gmail.com>
2023-04-04 12:26:30 +0100

    regular update

--- bin/scripts/install_mamba_jupy.sh
+++ bin/scripts/install_mamba_jupy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# to run one-off setting of jupyter notebook with mambalics
 
-echo "bagr"
-echo "buldozer" > ~/.jupy_installed
+if [ -d ~/.local/share/jupyter/kernels/mambalics ]; then
+ echo "the settings are already existing. Do not run this twice please"
+ exit
+fi
+
+mkdir -p ~/.local/share/jupyter/kernels/mambalics
+ln -s $LiCSARpath/misc/jupykernel.json ~/.local/share/jupyter/kernels/mambalics/kernel.json
+
+#echo "bagr"
+#echo "buldozer" > ~/.jupy_installed
