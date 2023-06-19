cde5c981222ade9b8d95e650719ce03ced560e59
laz048 <milan.lazecky@vsb.cz>
2023-06-19 15:12:51 +0100

    regular update

--- bin/scripts/test_vh_frame.sh
+++ bin/scripts/test_vh_frame.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 echo "one off test for VH data"
-frame=050D_05049_060600
-sdate=20141001
-#sdate=20230101
-edate=20230520
+echo "it will autodownload missing data in given time range, and then process VH layers of only epochs for which we have LUT files already."
 
+if [ -z $1 ]; then
+ echo "parameters: frame [DATEIN] [DATEOUT]"
+ echo "(use dates as e.g 20141001 20230512)"
+ exit
+fi
+frame=$1
+sdate=20141001
+edate=20290520
+if [ ! -z $2 ]; then sdate=$2; fi
+if [ ! -z $3 ]; then edate=$3; fi
 #mkdir -p $BATCH_CACHE_DIR/VHTEST
 #cd $BATCH_CACHE_DIR/VHTEST
 #origba=$BATCH_CACHE_DIR
 #export BATCH_CACHE_DIR=`pwd`
 
 if [ -d $BATCH_CACHE_DIR/$frame ]; then
-echo "careful, this frame already exists"
+echo "careful, this frame already exists. moving data to XXX.orig folders"
 mv $BATCH_CACHE_DIR/$frame/RSLC $BATCH_CACHE_DIR/$frame/RSLC.orig
 mv $BATCH_CACHE_DIR/$frame/SLC $BATCH_CACHE_DIR/$frame/SLC.orig
 fi
 
 licsar_make_frame.sh -n -f -P $frame 1 1 ${sdate:0:4}-${sdate:4:2}-${sdate:6:2} ${edate:0:4}-${edate:4:2}-${edate:6:2}
-touch $BATCH_CACHE_DIR/$frame/lmf_locked
 touch $BATCH_CACHE_DIR/$frame/lmf_locked
 
 cd $BATCH_CACHE_DIR/$frame
@@ -50,5 +56,7 @@ LiCSAR_01_mk_images.py -n -f $frame -d . -l luts.txt -s $sdate -e $edate -a 4 -r
 # run coreg (will use recoreg with LUTs)
 LiCSAR_02_coreg.py -f $frame -d . -i -m $m
 
-echo "done"
+rm $BATCH_CACHE_DIR/$frame/lmf_locked
+
+echo "done, see following directory: "
 pwd
