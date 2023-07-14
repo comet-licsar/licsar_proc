ea40057b5b9fabd6cb183b388e1b22c31b997ba5
laz048 <milan.lazecky@vsb.cz>
2023-07-14 14:05:34 +0100

    regular update

--- bin/scripts/create_bovl_ifg.sh
+++ bin/scripts/create_bovl_ifg.sh
@@ -81,11 +81,11 @@ if [ $filterit == 1 ]; then
  echo "filtering"
  #5.2 Make adf double difference 
  #adf IFG/$pair/ddiff.adf IFG/$pair/ddiff.adf.adf IFG/$pair/ddiff.adf.adf.cc $width 1 - - - - - -
- adf IFG/$pair/ddiff IFG/$pair/ddiff.adf IFG/$pair/ddiff_coh_adf $width 1 - - - - - -
+ adf IFG/$pair/ddiff IFG/$pair/ddiff.adf IFG/$pair/ddiff_coh_adf $width 1 - - - - - -  >/dev/null
 fi
  #6. To create coherence
  # width=3447 
-cc_wave IFG/$pair/ddiff RSLC/$m/$m.rslc.mli RSLC/$s/$s.rslc.mli IFG/$pair/ddiff_coh $width
+cc_wave IFG/$pair/ddiff RSLC/$m/$m.rslc.mli RSLC/$s/$s.rslc.mli IFG/$pair/ddiff_coh $width  >/dev/null
 
 
 #7. create geotiffs
@@ -95,24 +95,25 @@ cc_wave IFG/$pair/ddiff RSLC/$m/$m.rslc.mli RSLC/$s/$s.rslc.mli IFG/$pair/ddiff_
 
 #
 if [ $filterit == 1 ]; then
- cpx_to_real IFG/$pair/ddiff.adf IFG/$pair/ddiff_pha_adf $width 4
+ cpx_to_real IFG/$pair/ddiff.adf IFG/$pair/ddiff_pha_adf $width 4  >/dev/null
 fi
-cpx_to_real IFG/$pair/ddiff IFG/$pair/ddiff_pha $width 4
+cpx_to_real IFG/$pair/ddiff IFG/$pair/ddiff_pha $width 4  >/dev/null
 
 mkdir -p GEOC/$pair
+echo "geocoding"
 if [ $filterit == 1 ]; then
- geocode_back IFG/$pair/ddiff_coh_adf $width geo/$master.lt_fine GEOC/$pair/ddiff_coh_adf.geo $widthgeo - 0 0
- geocode_back IFG/$pair/ddiff_pha_adf $width geo/$master.lt_fine GEOC/$pair/ddiff_pha_adf.geo $widthgeo - 0 0
+ geocode_back IFG/$pair/ddiff_coh_adf $width geo/$master.lt_fine GEOC/$pair/ddiff_coh_adf.geo $widthgeo - 0 0  >/dev/null
+ geocode_back IFG/$pair/ddiff_pha_adf $width geo/$master.lt_fine GEOC/$pair/ddiff_pha_adf.geo $widthgeo - 0 0  >/dev/null
 fi
-geocode_back IFG/$pair/ddiff_coh $width geo/$master.lt_fine GEOC/$pair/ddiff_coh.geo $widthgeo - 0 0
-geocode_back IFG/$pair/ddiff_pha $width geo/$master.lt_fine GEOC/$pair/ddiff_pha.geo $widthgeo - 0 0
+geocode_back IFG/$pair/ddiff_coh $width geo/$master.lt_fine GEOC/$pair/ddiff_coh.geo $widthgeo - 0 0  >/dev/null
+geocode_back IFG/$pair/ddiff_pha $width geo/$master.lt_fine GEOC/$pair/ddiff_pha.geo $widthgeo - 0 0  >/dev/null
 #geocode_back IFG/$pair/ddiff_pha.adf $width geo/$master.lt_fine IFG/$pair/ddiff_pha.adf.geo $widthgeo - 0 0
 if [ $filterit == 1 ]; then
- data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_coh_adf.geo 2 GEOC/$pair/$pair.geo.bovldiff.adf.cc.tif 0.0
- data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_pha_adf.geo 2 GEOC/$pair/$pair.geo.bovldiff.adf.tif 0.0
+ data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_coh_adf.geo 2 GEOC/$pair/$pair.geo.bovldiff.adf.cc.tif 0.0  >/dev/null
+ data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_pha_adf.geo 2 GEOC/$pair/$pair.geo.bovldiff.adf.tif 0.0  >/dev/null
 fi
-data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_coh.geo 2 GEOC/$pair/$pair.geo.bovldiff.cc.tif 0.0
-data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_pha.geo 2 GEOC/$pair/$pair.geo.bovldiff.tif 0.0
+data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_coh.geo 2 GEOC/$pair/$pair.geo.bovldiff.cc.tif 0.0  >/dev/null
+data2geotiff geo/EQA.dem_par GEOC/$pair/ddiff_pha.geo 2 GEOC/$pair/$pair.geo.bovldiff.tif 0.0  >/dev/null
 #data2geotiff geo/EQA.dem_par IFG/$pair/ddiff_pha.adf.geo 2 GEOC/$pair/$pair.geo.bovldiff.tif 0.0
 
 ## cc
