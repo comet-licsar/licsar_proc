8c6f1f4a70896b19115bf1cb594313bf77b953df
laz048 <milan.lazecky@vsb.cz>
2023-07-27 11:46:26 +0100

    regular update

--- python/licsar_volc_ifgs.py
+++ python/licsar_volc_ifgs.py
@@ -10,6 +10,12 @@ except:
     print('volcid ..... ID of volcano based on Powell list.')
     print('             see https://volcano.si.edu/search_volcano.cfm')
     print('             or import volcdb; volcdb.find_volcano_by_name()')
+    print(' ')
+    print('This will process all hires frames related to the given volcano, creating standard set of interferograms.')
+    print('To fine tune the ifg network, you may check "bjobs" and when all related jobs are finished, you may want to \
+    rerun (in the $BATCH_CACHE_DIR/subsets/xxx/xxx) the command:')
+    print('framebatch_gapfill.sh -l -P -i ifgs.list -o 5 180 $rglks $azlks')
+    print('(if you have your own ifgs.list) - note rglks/azlks should be written on command line, but probably this is 13 2')
     exit()
 
 '''
