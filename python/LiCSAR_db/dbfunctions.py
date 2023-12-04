3e5e7f11fd296962a96f1ab9826d649d942743b3
laz048 <milan.lazecky@vsb.cz>
2023-12-04 13:27:47 +0000

    regular update

--- python/LiCSAR_db/dbfunctions.py
+++ python/LiCSAR_db/dbfunctions.py
@@ -27,7 +27,10 @@ def Conn_sqlalchemy(dbname='licsar_live'):
     
     engine = create_engine(
     # 'mysql+pymysql://{usr}:{psswd}@{hst}/{dbname}?charset=utf8'.format(
-    'mysql+pymysql://{usr}:{psswd}@{hst}/{dbname}?charset=utf-8'.format(
+    # 12/2023 - after JASMIN updates, there are some servers using utf8 and other utf-8 in the mysql lib settings
+    #'mysql+pymysql://{usr}:{psswd}@{hst}/{dbname}?charset=utf-8'.format(
+    # using the wrong returns ugly error. Fortunately... things work ok without specifying anything....
+    'mysql+pymysql://{usr}:{psswd}@{hst}/{dbname}'.format(
         usr=sqluser,
         psswd=sqlpass,
         hst=sqlhost,
