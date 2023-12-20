32f0fb472fb581b7003e51c5fecb78cffa9e0e4d
espiritocz <espiritocz@gmail.com>
2023-12-20 21:17:54 +0000

    Update volcdb.py
    new version of sqlalchemy (and pymysql)

--- python/LiCSAR_db/volcdb.py
+++ python/LiCSAR_db/volcdb.py
@@ -8,6 +8,7 @@ import pandas as pd
 try:
     from LiCSquery import *
     from dbfunctions import Conn_sqlalchemy
+    from sqlalchemy import text
 except:
     print('error loading LicsInfo tools - volcdb is quite useless then')
 
@@ -85,7 +86,7 @@ def get_volc_info(volcid=None):
         cond = ''
     sql = "select volc_id,name,lat,lon,alt,priority, ST_AsBinary(geometry) as geom from volcanoes"+cond+";"
     engine=Conn_sqlalchemy()
-    a = gpd.GeoDataFrame.from_postgis(sql, engine, geom_col='geom' )
+    a = gpd.GeoDataFrame.from_postgis(text(sql), engine, geom_col='geom' )
     #a = do_pd_query(sql)
     #a['geometry'] = a.geometry.apply(wkt.loads)
     return a
@@ -370,7 +371,8 @@ def get_volclips_gpd(vid=None):
     #sql = "SELECT ST_AsBinary(geometry) as geom from volclips {0};".format(cond)
     sql = "SELECT v.volc_id,v.name,vc.vid,ST_AsBinary(vc.geometry) as geom from volclips vc inner join volclip2volcs vf on vf.vid=vc.vid inner join volcanoes v on vf.volc_id=v.volc_id {0};".format(cond)
     engine=Conn_sqlalchemy()
-    volclips = gpd.GeoDataFrame.from_postgis(sql, engine, geom_col='geom' )
+    #volclips = gpd.GeoDataFrame.from_postgis(sql, engine, geom_col='geom' )
+    volclips = gpd.GeoDataFrame.from_postgis(text(sql), engine, geom_col='geom')
     return volclips
 
 
