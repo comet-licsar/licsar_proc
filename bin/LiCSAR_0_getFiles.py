cbbd32d19027178dbe1d6d3f96d2e0cfed51e802
Milan Lazecky <espiritocz@gmail.com>
2024-08-08 11:55:19 +0100

    regular update

--- bin/LiCSAR_0_getFiles.py
+++ bin/LiCSAR_0_getFiles.py
@@ -120,7 +120,7 @@ def main(argv=None):
     batchMode = 'M'
     startDate = dt.datetime.strptime('2016-01-01','%Y-%m-%d')
     endDate = dt.datetime.now()
-    retentionDate = dt.datetime.now()+dt.timedelta(14)
+    retentionDate = (dt.datetime.now()+dt.timedelta(14)).strftime("%Y-%m-%d")
     notify = False
     userEmail = None
 
