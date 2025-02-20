85a1fb0a8d1555c373b207fbf2b0160352049db9
espiritocz <espiritocz@gmail.com>
2025-02-20 13:58:34 +0000

    better fix geoc errs

--- bin/scripts/fix_geocoding_frame.sh
+++ bin/scripts/fix_geocoding_frame.sh
@@ -20,43 +20,78 @@ cdpub $frame
 hgt=`pwd`/metadata/$frame.geo.hgt.tif
 
 coordhgt=`gdalinfo $hgt | grep Center | gawk {'print $3'} | cut -d ',' -f1`
+sizehgt=`gdalinfo $hgt | grep Size | head -n 1 | sed 's/ //g'`
 
 for tif in `ls metadata/*tif`; do
  coord=`gdalinfo $tif | grep Center | gawk {'print $3'} | cut -d ',' -f1`
  if [ ! $coordhgt == $coord ]; then
+   corit=1
+ else
+   sizetif=`gdalinfo $tif | grep Size | head -n 1 | sed 's/ //g'`
+   if [ ! $sizehgt == $sizetif ]; then
+     corit=1
+   else
+     corit=0
+   fi
+ fi
+ if [ $corit == 1 ]; then
    echo Correcting $tif
    gdalwarp2match.py $tif $hgt temp_warped.tif >/dev/null 2>/dev/null
    rm -f $tif
    gdal_translate -of GTiff -co COMPRESS=DEFLATE temp_warped.tif $tif >/dev/null 2>/dev/null
    rm temp_warped.tif
+ else
+  echo "this is ok: "$tif
  fi
 done
 
 for epoch in `ls epochs | grep 20`; do
  for tif in `ls epochs/$epoch/*tif`; do
-  coord=`gdalinfo $tif | grep Center | gawk {'print $3'} | cut -d ',' -f1`
-  if [ ! $coordhgt == $coord ]; then
-    echo Correcting $tif
-    gdalwarp2match.py $tif $hgt temp_warped.tif >/dev/null 2>/dev/null
-    rm -f $tif
-    gdal_translate -of GTiff -co COMPRESS=DEFLATE temp_warped.tif $tif >/dev/null 2>/dev/null
-    rm temp_warped.tif
-  fi
+ coord=`gdalinfo $tif | grep Center | gawk {'print $3'} | cut -d ',' -f1`
+ if [ ! $coordhgt == $coord ]; then
+   corit=1
+ else
+   sizetif=`gdalinfo $tif | grep Size | head -n 1 | sed 's/ //g'`
+   if [ ! $sizehgt == $sizetif ]; then
+     corit=1
+   else
+     corit=0
+   fi
+ fi
+ if [ $corit == 1 ]; then
+   echo Correcting $tif
+   gdalwarp2match.py $tif $hgt temp_warped.tif >/dev/null 2>/dev/null
+   rm -f $tif
+   gdal_translate -of GTiff -co COMPRESS=DEFLATE temp_warped.tif $tif >/dev/null 2>/dev/null
+   rm temp_warped.tif
+ else
+  echo "this is ok: "$tif
+ fi
  done
 done
 
 
 for pair in `ls interferograms | grep 20`; do
  for tif in `ls interferograms/$pair/*tif`; do
-   coord=`gdalinfo $tif | grep Center | gawk {'print $3'} | cut -d ',' -f1`
-   if [ ! $coordhgt == $coord ]; then
-     echo Correcting $tif
-     gdalwarp2match.py $tif $hgt temp_warped.tif >/dev/null 2>/dev/null
-     rm -f $tif
-     gdal_translate -of GTiff -co COMPRESS=DEFLATE temp_warped.tif $tif >/dev/null 2>/dev/null
-     rm temp_warped.tif
+ coord=`gdalinfo $tif | grep Center | gawk {'print $3'} | cut -d ',' -f1`
+ if [ ! $coordhgt == $coord ]; then
+   corit=1
+ else
+   sizetif=`gdalinfo $tif | grep Size | head -n 1 | sed 's/ //g'`
+   if [ ! $sizehgt == $sizetif ]; then
+     corit=1
    else
-     echo "this is ok: "$tif
+     corit=0
    fi
+ fi
+ if [ $corit == 1 ]; then
+   echo Correcting $tif
+   gdalwarp2match.py $tif $hgt temp_warped.tif >/dev/null 2>/dev/null
+   rm -f $tif
+   gdal_translate -of GTiff -co COMPRESS=DEFLATE temp_warped.tif $tif >/dev/null 2>/dev/null
+   rm temp_warped.tif
+ else
+  echo "this is ok: "$tif
+ fi
  done
 done
