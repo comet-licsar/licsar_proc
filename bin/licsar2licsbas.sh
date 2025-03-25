ebdda38b280656feb08622909a75c4877598f950
[mnergizci] <eemne@leeds.ac.uk>
2025-03-25 23:53:55 +0000

    sbovl flag for step 14

--- bin/licsar2licsbas.sh
+++ bin/licsar2licsbas.sh
@@ -1103,6 +1103,7 @@ if [ $sbovl -gt 0 ]; then
  sed -i 's/p11_sbovl=\"n\"/p11_sbovl=\"y\"/' batch_LiCSBAS.sh
  sed -i 's/p120_sbovl=\"n\"/p120_sbovl=\"y\"/' batch_LiCSBAS.sh
  sed -i 's/p13_sbovl=\"n\"/p13_sbovl=\"y\"/' batch_LiCSBAS.sh
+ sed -i 's/p14_sbovl=\"n\"/p14_sbovl=\"y\"/' batch_LiCSBAS.sh
  sed -i 's/p15_sbovl=\"n\"/p15_sbovl=\"y\"/' batch_LiCSBAS.sh
  sed -i 's/p16_sbovl=\"n\"/p16_sbovl=\"y\"/' batch_LiCSBAS.sh  #TODO: check the step16 filtering options for SBOI discuss with Milan.
 fi
