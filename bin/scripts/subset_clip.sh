27c4decd9e2367f8fcd28809c9f07732769a13e5
espiritocz <espiritocz@gmail.com>
2025-09-24 23:13:34 +0100

    Update subset_clip.sh

--- bin/scripts/subset_clip.sh
+++ bin/scripts/subset_clip.sh
@@ -2,30 +2,48 @@
 
 # this script can also work in full frame but it is set to allow subset clipping to even smaller area
 # e.g. to find corner reflector where we want to load intensities into datacube
+# usage with params:
+# subset_clip.sh lon lat foldername [half range] [half azi]
 
+# must be run inside the subset directory, e.g. in
+# /gws/nopw/j04/nceo_geohazards_vol1/projects/LiCS/proc/current/subsets/kladno/095D
 makemlinc=1  # this will create MLI NetCDF file.. that should be actually converted to amplitude (and put dB in)
 
-ls /gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4
-cd /gws/nopw/j04/nceo_geohazards_vol1/projects/LiCS/proc/current/subsets/kladno/095D
+#ls /gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4
+#cd /gws/nopw/j04/nceo_geohazards_vol1/projects/LiCS/proc/current/subsets/kladno/095D
 centerlon=14.00393208465662
 centerlat=50.12668894637102
 halflenrg=14
 halflenazi=3
-outdir=subset.kz4
+locname=kz4
 
+# let's get it from params:
+if [ ! -z $3 ]; the
+centerlon=$1
+centerlat=$2
+locname=$3
+if [ ! -z $5 ]; then
+  halflenrg=$4
+  halflenazi=$5
+fi
+fi
+outdir=subset.$locname
+relorb=`pwd | rev | cut -d '/' -f 1 | rev`
 
 mkdir -p $outdir/RSLC
+echo "cd "`pwd`"; subset_clip.sh "$centerlon" "$centerlat" "$locname" "$halflenrg" "$halflenazi > $outdir/command.in
 m=`ls SLC | head -n 1`
 h=`ls GEOC.meta*/*geo.hgt.tif | head -n 1`
 
 slc=SLC/$m/$m.slc
 slcpar=SLC/$m/$m.slc.par
-if [ ! -f tmp.subset.centre ]; then
- hei=`python3 -c "import rioxarray;a=rioxarray.open_rasterio('"$h"'); aa=a.sel(x="$centerlon",y="$centerlat", method='nearest'); print(aa.values[0])"`
- coord_to_sarpix $slcpar - - $centerlat $centerlon $hei | grep "SLC/MLI range, azimuth pixel (int)" > tmp.subset.centre
-fi
-azipx=`cat tmp.subset.centre | rev | gawk {'print $1'} | rev`
-rgpx=`cat tmp.subset.centre | rev | gawk {'print $2'} | rev`
+#if [ ! -f tmp.subset.centre ]; then
+ #hei=`python3 -c "import rioxarray;a=rioxarray.open_rasterio('"$h"'); aa=a.sel(x="$centerlon",y="$centerlat", method='nearest'); print(aa.values[0])"`
+ hei=`gdallocationinfo -geoloc -valonly $h $centerlon $centerlat`
+ coord_to_sarpix $slcpar - - $centerlat $centerlon $hei | grep "SLC/MLI range, azimuth pixel (int)" > $outdir/tmp.subset.centre
+#fi
+azipx=`cat $outdir/tmp.subset.centre | rev | gawk {'print $1'} | rev`
+rgpx=`cat $outdir/tmp.subset.centre | rev | gawk {'print $2'} | rev`
 
 let azi2=azipx+$halflenazi
 let azi1=azipx-$halflenazi
@@ -35,8 +53,9 @@ let rg2=rgpx+$halflenrg
 let rg1=rgpx-$halflenrg
 let rgdiff=rg2-rg1+1
 
-if [ $makemlinc == 1 ]; then
+
 # 1. generate the small size mlis (lazy approach. not effective. still ok)
+echo "clipping rslcs"
 for r in `ls RSLC`; do
  # TODO from here
  #echo "clipping reference epoch"
@@ -45,66 +64,101 @@ for r in `ls RSLC`; do
  if [ ! -f $outdir/RSLC/$r/$r.rslc ]; then
   SLC_copy RSLC/$r/$r.rslc RSLC/$r/$r.rslc.par $outdir/RSLC/$r/$r.rslc $outdir/RSLC/$r/$r.rslc.par - - $rg1 $rgdiff $azi1 $azidiff - - >/dev/null 2>/dev/null
  fi
+done
+
+if [ $makemlinc == 1 ]; then
+  echo "generating mlis"
+for r in `ls RSLC`; do
  if [ ! -f $outdir/RSLC/$r/$r.rslc.mli ]; then
-   sleep 2
   multi_look $outdir/RSLC/$r/$r.rslc $outdir/RSLC/$r/$r.rslc.par $outdir/RSLC/$r/$r.rslc.mli $outdir/RSLC/$r/$r.rslc.mli.par 1 1 >/dev/null 2>/dev/null
  fi
 done
-# now load that to netcdf (and convert to amplitude)
+# radcal
+  echo "basic radiometric calibration of mlis"
+for r in `ls RSLC`; do
+ if [ ! -f $outdir/RSLC/$r/$r.rslc.mli.calibrated ]; then
+  radcal_MLI $outdir/RSLC/$r/$r.rslc.mli $outdir/RSLC/$r/$r.rslc.mli.par - $outdir/RSLC/$r/$r.rslc.mli.calibrated - 1 >/dev/null 2>/dev/null
+ fi
+done
 
-cd $outdir
-# python...
+# now load that to netcdf (and convert to amplitude)
+cat << EOF >$outdir/tonc.py
 import os
 import numpy as np
 import xarray as xr
 import datetime as dt
 
-width=29 #$rgdiff # 29
-length=7 #$azidiff # 7
+width=int(1+2*$halflenrg)  # 29 #$rgdiff # 29
+length=int(1+2*$halflenazi)  #7 #$azidiff # 7
 # for track 95:
 # nano subset.kz4/RSLC/$m/$m.rslc.mli.par
 
-time95 = '05 18 2.04720'
-time146= '16:51:59'
-time22='05 09 48.65'
-time44='17 00 13.78'
+def str2dt(strtime):
+    #strtime = ['2024', '09', '08', '05', '09', '48.65361']
+    # Convert to appropriate types
+    year, month, day = map(int, strtime[:3])
+    hour, minute = map(int, strtime[3:5])
+    #second = float(strtime[5])
+    second = int(strtime[5].split('.')[0])
+    # Create datetime object
+    return dt.datetime(year, month, day, hour, minute, second) #int(second), int((second % 1) * 1e6))
 
-h = 5
-m = 18
-s = int(2.042720)
-h=16
-m=51
-s=59
-#22
-h=5
-m=9
-s=49
-#44
-h=17
-m=0
-s=14
+
+def mlipartime(mlipar):
+    strtime=misc.grep1line('date',mlipar).split()[1:]
+    return str2dt(strtime)
+
 
 epochs = []
 data = []
+todb = False
 for epoch in os.listdir('RSLC'):
     print(epoch)
-    mli='RSLC/'+epoch+'/'+epoch+'.rslc.mli'
+    mli='RSLC/'+epoch+'/'+epoch+'.rslc.mli.calibrated'
+    mlipar='RSLC/'+epoch+'/'+epoch+'.rslc.mli.par'
+    if not os.path.exists(mli):
+        mli='RSLC/'+epoch+'/'+epoch+'.rslc.mli'
     if os.path.exists(mli):
+        print(mli)
         a=np.fromfile(mli, dtype=np.float32)
-        ampdb=10*np.log10(np.sqrt(a.byteswap())).reshape((length, width))
+        if todb:
+            amp=10*np.log10(np.sqrt(a.byteswap())).reshape((length, width))
+        else:
+            amp=np.sqrt(a.byteswap()).reshape((length, width))
         #if not np.isinf(ampdb):
-        epochdt = dt.datetime(int(epoch[:4]), int(epoch[4:6]), int(epoch[6:]), h, m, s)
+        epochdt = mlipartime(mlipar)
+        # epochdt = dt.datetime(int(epoch[:4]), int(epoch[4:6]), int(epoch[6:]), h, m, s)
         epochs.append(epochdt)
-        data.append(ampdb)
+        data.append(amp)
 
 
 bb=xr.DataArray(data=data, dims=['epoch','azi','rg'], coords=dict(azi=np.arange(length), rg=np.arange(width), epoch=epochs))
-bb=xr.Dataset(data_vars=dict(amplitude_dB=bb))
-outnc = '/gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4/095c.nc'
-outnc = '/gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4/146.nc'
-outnc = '/gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4/44.nc'
+bb=xr.Dataset(data_vars=dict(amplitude=bb))
+outnc = '$locname.$relorb.amp.nc'
+# bb=xr.Dataset(data_vars=dict(amplitude_dB=bb))
+#outnc = '/gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4/095c.nc'
+#outnc = '/gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4/146.nc'
+#outnc = '/gws/nopw/j04/nceo_geohazards_vol1/public/shared/temp/earmla/kladno/kz4/44.nc'
 bb.to_netcdf(outnc)
 
-
+EOF
+cd $outdir; python3 tonc.py
 
 fi
+
+
+exit
+
+
+# how i did kladno:
+cd /gws/nopw/j04/nceo_geohazards_vol1/projects/LiCS/proc/current/subsets/kladno
+python3
+import pandas as pd
+import os
+csv='odrazece.csv'
+df=pd.read_csv(csv, index_col=0)
+for relorb in os.listdir():
+    relorbpath = os.path.join(os.getcwd(), relorb)
+    for i,row in df.iterrows():
+        cmd="cd {0}; subset_clip.sh {1} {2} {3}".format(relorbpath, str(row['lon']), str(row['lat']), row['misto'])
+        print(cmd)
